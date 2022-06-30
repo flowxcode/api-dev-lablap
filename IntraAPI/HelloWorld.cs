54aae8de3a8c32a69824bb9bcf76bec0d1a5e217
Florian Isopp <florian.johannes.isopp@gmail.com>
2022-06-30 13:37:08 +0200

    smart contracts init

---
+++ IntraAPI/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿//using Neo.SmartContract.Framework;
+//using Neo.SmartContract.Framework.Services.Neo;
+//using System;
+//namespace HelloWorld
+//{
+//    public class HelloWorld : SmartContract
+//    {
+//        private const string test_str = "Hello World";
+//        public static String Main(string operation, object[] args)
+//        {
+//            Storage.Put("Hello", "World");
+//            return test_str;
+//        }
+//    }
+//}
